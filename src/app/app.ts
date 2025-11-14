59272080258e2b458abbda031f95eacd3ce97afc
Hermawan Saputra <126874286+hermwnS@users.noreply.github.com>
2025-11-14 14:42:59 +0700

    menambahkan model item dan injectable di app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Item } from './model/item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppService {
+  
+}
+
 export class App {
   protected readonly title = signal('belajar_angular');
 }
